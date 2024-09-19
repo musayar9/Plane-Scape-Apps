482e424a624368287802210a90359496f68933a4
Musa Sayar <musasayar67@gmail.com>
2024-09-19 14:25:31 +0300

    created bookFlight model and created bookFlightController

---
+++ server/controllers/bookFlightController.js
@@ -0,0 +1,23 @@
+const BookFlight = require("../models/BookFlightModel");
+
+const createBookFlight = async (req, res, next) => {
+  const bookFlight = new BookFlight({ ...req.body });
+
+  try {
+    await bookFlight.save();
+    res.status(201).json({ message: "flight booked", bookFlight });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getBookFlight = async (req, res, next) => {
+  const bookFlight = await BookFlight.find({}).sort({ createdAt: -1 });
+  try {
+    res.status(200).json(bookFlight);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { createBookFlight, getBookFlight };
---
+++ server/models/bookFlightModel.js
@@ -0,0 +1,25 @@
+const mongoose  = require("mongoose")
+
+
+const BookFlightSchema = new mongoose.Schema({
+  airlineCode: { type: Number, required: true },
+  estimatedLandingTime: { type: Date, required: true },
+  flightDirection: { type: String, required: true },
+  flightName: { type: String, required: true },
+  flightNumber: { type: Number, required: true },
+  prefixIATA: { type: String, required: true },
+  prefixICAO: { type: String, required: true },
+  price:{type:String,},
+  publicFlightState: {
+    flightStates: [{ type: String }],
+  },
+  route: {
+    destinations: [{ type: String, required: true }], 
+    eu: { type: String, required: true }, 
+    visa: { type: Boolean, required: true },
+  },
+  scheduleDateTime: { type: Date, required: true },
+});
+
+const BookFlight = mongoose.model("BookFlight", BookFlightSchema);
+module.exports = BookFlight
